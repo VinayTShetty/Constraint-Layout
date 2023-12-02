3af0db28074deda99b9f25bed5b82d577f5b4e90
VinayTShetty <vinaytshetty@gmail.com>
2023-12-02 11:45:13 +0530

    Constraint Layout Example Demo

---
+++ app/src/main/java/com/education/layout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.education.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.demo_five);
+    }
+}
